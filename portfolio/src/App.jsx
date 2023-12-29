9f9e42f8f0b8aade93cf61aecd1d5708c4b183bf
Eitan Fire <eitanfire@gmail.com>
2023-12-29 12:24:03 -0700

    add project titles

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <span>My Latest Projects</span>
+      <section>🚀 Teach League</section>
+      <section>👨🏻‍🏫 Class Website</section>
+      <section>👻 Costume Contest Scoreboard</section>
+    </>
+  );
+}
+
+export default App
