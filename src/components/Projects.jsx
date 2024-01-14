da78f44e792bc7be6e6d7db858134aa26695b1be
Eitan Fire <eitanfire@gmail.com>
2024-01-13 20:55:29 -0700

    make responsive for screen size

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -10,8 +10,8 @@ const Projects = () => {
     <Container>
       <h2 className="title">🚀 Teach League</h2>
       <Row className="mb-3">
-        <Col className="col col-xs-1" sm="1">
-          <div className="project-links">
+        <Col className="col col-xl-2">
+          <div className="project-links d-md-none">
             <a
               href="https://teach-league.web.app/"
               target="_blank"
@@ -21,7 +21,29 @@ const Projects = () => {
             </a>
           </div>
           <br></br>
-          <div className="project-links">
+          <div className="project-links d-md-none d-block-lg">
+            <a
+              // className="btn btn-social-icon btn-github"
+              href="https://github.com/eitanfire/legendary-quest.git"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <i className="fa fa-github"></i> GitHub
+            </a>
+          </div>
+        </Col>
+        <Col className="col col-xl-2">
+          <div className="project-links-md d-none d-md-block d-lg-none hstack">
+            <a
+              href="https://teach-league.web.app/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <i className="fa fa-circle-play"></i> Live Demo
+            </a>
+          </div>
+          <br></br>
+          <div className="project-links-md d-none d-md-block d-lg-none hstack">
             <a
               // className="btn btn-social-icon btn-github"
               href="https://github.com/eitanfire/legendary-quest.git"
@@ -35,7 +57,7 @@ const Projects = () => {
         <Col
         // className="vstack"
         >
-          <Col className="col col-xs-2" sm="2">
+          <Col className="col col-2 d-md-none d-xl-block">
             <img
               className="screenshots"
               src={TLLight}
@@ -43,7 +65,7 @@ const Projects = () => {
             ></img>
           </Col>
           <br></br>
-          <Col className="col col-xs-2" sm="2">
+          <Col className="col col-2 d-md-none d-xl-block">
             <img
               className="screenshots"
               src={TL}
@@ -51,9 +73,26 @@ const Projects = () => {
             ></img>
           </Col>
         </Col>
+        <Col className="hstack">
+          <Col className="d-none d-md-block d-xl-none">
+            <img
+              className="screenshots-md"
+              src={TLLight}
+              alt="Teach League screenshot"
+            ></img>
+          </Col>
+          <br></br>
+          <Col className="d-none d-md-block d-xl-none">
+            <img
+              className="screenshots-md"
+              src={TL}
+              alt="Teach League screenshot"
+            ></img>
+          </Col>
+        </Col>
         {/* </Row>
       <Row> */}
-        <Col className="description col col-xs-7" sm="8">
+        <Col className="description col-12 col-xl-7">
           <p>
             As a teacher with 14 years of experience I am eager to offer support
             and mentorship to other educators in the field. This web app shares
@@ -124,40 +163,42 @@ const Projects = () => {
             shows important school events and also shows the due dates for any
             assignments their teachers assign on Google Classroom. The carousel
             raises community spirit by "shouting out" students and staff for
-            taking pride in their community
+            taking pride in their community.
           </p>
         </Col>
       </Row>
+      <h2 className="title">👻 Costume Contest Scoreboard</h2>
       <Row className="mb-3">
-        <h2 className="title">👻 Costume Contest Scoreboard</h2>
-        <Col className="col col-xs-8" sm="8">
-          <img
-            className="screenshots"
-            src={Leaderboard}
-            alt="Class website screenshot"
-          ></img>
-        </Col>
         <Col className="col col-xs-2" sm="2">
-          <a
-            href="https://teach-league.web.app/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            link
-          </a>
-          <div>
+          <div className="project-links">
             <a
+              href="https://teach-league.web.app/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <i className="fa fa-circle-play"></i> Live Demo
+            </a>
+          </div>
+          <br></br>
+          <div className="project-links">
+            <a
+              // className="btn btn-social-icon btn-github"
               href="https://github.com/eitanfire/legendary-quest.git"
               target="_blank"
               rel="noreferrer"
             >
-              GitHub
+              <i className="fa fa-github"></i> GitHub
             </a>
           </div>
         </Col>
-      </Row>
-      <Row className="description">
-        <Col className="col col-xs-6">
+        <Col className="col col-xs-2" sm="2">
+          <img
+            className="screenshots"
+            src={Leaderboard}
+            alt="Scoreboard screenshot"
+          ></img>
+        </Col>
+        <Col className="description col col-xs-7" sm="7">
           {" "}
           <p>
             I aim to write clean code in a functional style but I won't
@@ -171,6 +212,27 @@ const Projects = () => {
             terrifying as having a school full of students tensely watching me
             debug a program to find out the winner of the costume contest!
           </p>
+        </Col>
+      </Row>
+      <Row className="mb-3">
+        <Col
+        // className="vstack"
+        >
+          <Col className="col col-xs-2" sm="2">
+            <img
+              className="screenshots"
+              src={TLLight}
+              alt="Teach League screenshot"
+            ></img>
+          </Col>
+          <br></br>
+          <Col className="col col-xs-2" sm="2">
+            <img
+              className="screenshots"
+              src={TL}
+              alt="Teach League screenshot"
+            ></img>
+          </Col>
         </Col>
       </Row>
     </Container>
