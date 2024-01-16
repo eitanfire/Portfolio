e1433d530dd426022efa9c479fea6c00de728b83
Eitan Fire <eitanfire@gmail.com>
2024-01-15 22:53:35 -0700

    update text

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,7 +1,7 @@
 import TL from "../assets/img/tl-ss.png";
 import TLLight from "../assets/img/tl-light-ss.png";
 import Class from "../assets/img/class-website-ss.png";
-import About from "../assets/img/class-website-about-me-ss.png";
+import About from "../assets/img/class-website-about-me-sm-ss.png";
 import Leaderboard from "../assets/img/leaderboard--ss.png";
 import { Container, Col, Row, Card, CardBody } from "reactstrap";
 
@@ -10,172 +10,8 @@ const Projects = () => {
     <Container>
       <h2 className="title">🚀 Teach League</h2>
       <Row className="mb-3">
-        {/* Old Styling*/}
-        <Row className="mb-3">
-          <Col className="col">
-            <div className="project-links d-md-none d-lg-block">
-              <a
-                href="https://teach-league.web.app/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <i className="fa fa-circle-play"></i> Live Demo
-              </a>
-            </div>
-            <br></br>
-            <div className="project-links d-md-none d-lg-block">
-              <a
-                // className="btn btn-social-icon btn-github"
-                href="https://github.com/eitanfire/legendary-quest.git"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <i className="fa fa-github"></i> GitHub
-              </a>
-            </div>
-          </Col>
-          <Col
-          // className="vstack"
-          >
-            <Col
-              className="col col-12 
-        hstack"
-            >
-              <div className="project-links-md d-none d-md-block d-lg-none">
-                <a
-                  href="https://teach-league.web.app/"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <i className="fa fa-circle-play"></i> Live Demo
-                </a>
-              </div>
-              <br></br>
-              <div className="project-links-md d-none d-md-block d-lg-none">
-                <a
-                  // className="btn btn-social-icon btn-github"
-                  href="https://github.com/eitanfire/legendary-quest.git"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <i className="fa fa-github"></i> GitHub
-                </a>
-              </div>
-            </Col>
-            <Col className="screenshot-container col d-md-none d-xl-block col-xl-2">
-              <Col>
-                <img
-                  className="screenshots"
-                  src={TLLight}
-                  alt="Teach League screenshot"
-                ></img>
-              </Col>
-              <br></br>
-              <Col>
-                <img
-                  className="screenshots"
-                  src={TL}
-                  alt="Teach League screenshot"
-                ></img>
-              </Col>
-            </Col>
-            <Col className="hstack">
-              <Col className="d-none d-md-block d-xl-none">
-                <img
-                  className="screenshots-md"
-                  src={TLLight}
-                  alt="Teach League screenshot"
-                ></img>
-              </Col>
-              <br></br>
-              <Col className="d-none d-md-block d-xl-none">
-                <img
-                  className="screenshots-md"
-                  src={TL}
-                  alt="Teach League screenshot"
-                ></img>
-              </Col>
-            </Col>
-          </Col>
-          {/* </Row>
-      <Row> */}
-          <Col className="description col col-12 col-xl-8">
-            <p>
-              As a teacher with 14 years of experience I am eager to offer
-              support and mentorship to other educators in the field. This web
-              app shares the high-quality curriculum I have built over the
-              years.
-            </p>
-            <p>
-              I help teachers stay sharp as professionals by curating
-              illuminating articles and ultimately to support their own voices.
-            </p>
-            <Col className="col">
-              <p>
-                I used the Google Classroom API to provide them with an
-                emergency lesson plan they can easily assign to their students.
-              </p>
-              <p></p>
-              <p>
-                Users can click on the light bulb to toggle between a light and
-                dark theme.
-              </p>
-            </Col>
-          </Col>
-        </Row>
-        {/* <h2 className="title">👨🏻‍🏫 Class Website</h2>
-        <Col className="col col-xs-8" sm="8">
-          <img
-            className="screenshots"
-            src={Class}
-            alt="Class website screenshot"
-          ></img>
-        </Col>
-        <Col className="col col-xs-2" sm="2">
-          <a
-            href="https://teach-league.web.app/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            link
-          </a>
-          <div>
-            <a
-              href="https://github.com/eitanfire/legendary-quest.git"
-              target="_blank"
-              rel="noreferrer"
-            >
-              GitHub
-            </a>
-          </div>
-        </Col>
-      </Row>
-      <Row className="description">
-        <Col className="col col-xs-6">
-          <p>
-            I created this class website to support my students in getting the
-            information they need to be successful in class. I used the date API
-            to conditionally render a component that lets them know if it is an
-            A day or a B day within the block schedule we follow. I leveraged
-            this same API to keep all other date related information seemlessly
-            updated such as the information foound it on the "About Me" page.
-          </p>
-        </Col>
-        <Col className="col col-xs-6">
-          <p>
-            I created an accordion that gives students quick access to
-            assignemnts and documents they will need for the day. The calendar
-            shows important school events and also shows the due dates for any
-            assignments their teachers assign on Google Classroom. The carousel
-            raises community spirit by "shouting out" students and staff for
-            taking pride in their community.
-          </p>
-        </Col>
-      </Row>
-      <h2 className="title">👻 Costume Contest Scoreboard</h2>
-      <Row className="mb-3">
-        <Col className="col col-xs-2" sm="2">
-          <div className="project-links">
+        <Col className="col">
+          <div className="project-links d-md-none d-lg-block">
             <a
               href="https://teach-league.web.app/"
               target="_blank"
@@ -185,7 +21,7 @@ const Projects = () => {
             </a>
           </div>
           <br></br>
-          <div className="project-links">
+          <div className="project-links d-md-none d-lg-block">
             <a
               // className="btn btn-social-icon btn-github"
               href="https://github.com/eitanfire/legendary-quest.git"
@@ -196,49 +32,314 @@ const Projects = () => {
             </a>
           </div>
         </Col>
-        <Col className="col col-xs-2" sm="2">
-          <img
-            className="screenshots"
-            src={Leaderboard}
-            alt="Scoreboard screenshot"
-          ></img>
+        <Col
+        // className="vstack"
+        >
+          <Col
+            className="col col-12 
+        hstack"
+          >
+            <div className="project-links-md d-none d-md-block d-lg-none">
+              <a
+                href="https://teach-league.web.app/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <i className="fa fa-circle-play"></i> Live Demo
+              </a>
+            </div>
+            <br></br>
+            <div className="project-links-md d-none d-md-block d-lg-none">
+              <a
+                // className="btn btn-social-icon btn-github"
+                href="https://github.com/eitanfire/legendary-quest.git"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <i className="fa fa-github"></i> GitHub
+              </a>
+            </div>
+          </Col>
+          <Col className="screenshot-container col d-md-none d-xl-block col-xl-2">
+            <Col>
+              <img
+                className="screenshots"
+                src={TLLight}
+                alt="Teach League screenshot"
+              ></img>
+            </Col>
+            <br></br>
+            <Col>
+              <img
+                className="screenshots"
+                src={TL}
+                alt="Teach League screenshot"
+              ></img>
+            </Col>
+          </Col>
+          <Col className="hstack">
+            <Col className="d-none d-md-block d-xl-none">
+              <img
+                className="screenshots-md"
+                src={TLLight}
+                alt="Teach League screenshot"
+              ></img>
+            </Col>
+            <br></br>
+            <Col className="d-none d-md-block d-xl-none">
+              <img
+                className="screenshots-md"
+                src={TL}
+                alt="Teach League screenshot"
+              ></img>
+            </Col>
+          </Col>
         </Col>
-        <Col className="description col col-xs-7" sm="7">
-          {" "}
+        {/* </Row>
+      <Row> */}
+        <Col className="description col col-12 col-xl-8">
           <p>
-            I aim to write clean code in a functional style but I won't
-            hesitaste to make use of class based components when working with
-            legacy code such as the Scoreboard which I started with a tutorial.
+            As a teacher with 14 years of experience, I am eager to offer
+            support and mentorship to my colleagues. This web app shares the
+            high-quality curriculum I have built over the years.
           </p>
-        </Col>
-        <Col className="col col-xs-6">
           <p>
-            As a teacher a whiteboard coding interview is not quite as
-            terrifying as having a school full of students tensely watching me
-            debug a program to find out the winner of the costume contest!
+            I help teachers stay sharp as professionals by curating illuminating
+            articles and ultimately to support their voices.
           </p>
+          <Col className="col">
+            <p>
+              I used the Google Classroom API to provide them with an emergency
+              lesson plan they can easily assign to their students.
+            </p>
+            <p></p>
+            <p>
+              Users can click on the light bulb to toggle between a light and
+              dark theme and this preference will be saved to the local storage
+              of their device so pages can be reloaded without reverting. This
+              website is optimized for the screen size found in the full range
+              of devices.
+            </p>
+          </Col>
         </Col>
       </Row>
+      <h2 className="title">👨🏻‍🏫 Class Website</h2>
       <Row className="mb-3">
         <Col
         // className="vstack"
         >
-          <Col className="col col-xs-2" sm="2">
-            <img
-              className="screenshots"
-              src={TLLight}
-              alt="Teach League screenshot"
-            ></img>
+          <Col className="description col col-12 col-xl-8">
+            <p>
+              I created this class website to support my students in getting the
+              information they need to be successful in class. I used the date
+              API to conditionally render a component that lets them know if it
+              is an A day or a B day within the block schedule we follow. I
+              leveraged this same API to keep all other date-related information
+              seamlessly updated such as the information found on the "About Me"
+              page. The layout of this web app adjusts responsively according to
+              screen size.
+            </p>
+            <p>
+              I created an accordion that gives students quick access to
+              assignments and documents they need for the day. The calendar
+              shows important school events and the due dates for any
+              assignments their teachers assign on Google Classroom. The
+              carousel raises community spirit by "shouting out" students and
+              staff for taking pride in their community.
+            </p>
           </Col>
+          <Col
+            className="col col-12 
+        hstack"
+          >
+            <Col className="screenshot-container col d-md-none d-xl-block col-xl-2">
+              <Col>
+                <img
+                  className="screenshots"
+                  src={Class}
+                  alt="Teach League screenshot"
+                ></img>
+              </Col>
+              <br></br>
+              <Col>
+                <img
+                  className="screenshots"
+                  src={About}
+                  alt="Teach League screenshot"
+                ></img>
+              </Col>
+            </Col>
+            <Col className="hstack">
+              <Col className="d-none d-md-block d-xl-none">
+                <img
+                  className="screenshots-md"
+                  src={Class}
+                  alt="Teach League screenshot"
+                ></img>
+              </Col>
+              <br></br>
+              <Col className="d-none d-md-block d-xl-none">
+                <img
+                  className="screenshots-md"
+                  src={About}
+                  alt="Teach League screenshot"
+                ></img>
+              </Col>
+            </Col>
+            <div className="project-links-md d-none d-md-block d-lg-none">
+              <a
+                href="https://teach-league.web.app/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <i className="fa fa-circle-play"></i> Live Demo
+              </a>
+            </div>
+            <br></br>
+            <div className="project-links-md d-none d-md-block d-lg-none">
+              <a
+                // className="btn btn-social-icon btn-github"
+                href="https://github.com/eitanfire/legendary-quest.git"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <i className="fa fa-github"></i> GitHub
+              </a>
+            </div>
+          </Col>
+        </Col>
+        <Col className="col">
+          <div className="project-links d-md-none d-lg-block">
+            <a
+              href="https://teach-league.web.app/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <i className="fa fa-circle-play"></i> Live Demo
+            </a>
+          </div>
           <br></br>
-          <Col className="col col-xs-2" sm="2">
-            <img
-              className="screenshots"
-              src={TL}
-              alt="Teach League screenshot"
-            ></img>
+          <div className="project-links d-md-none d-lg-block">
+            <a
+              // className="btn btn-social-icon btn-github"
+              href="https://github.com/eitanfire/legendary-quest.git"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <i className="fa fa-github"></i> GitHub
+            </a>
+          </div>
+        </Col>
+        <Col
+          className="col col-12 
+        hstack"
+        >
+          <div className="project-links-md d-none d-md-block d-lg-none">
+            <a
+              href="https://teach-league.web.app/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <i className="fa fa-circle-play"></i> Live Demo
+            </a>
+          </div>
+          <br></br>
+          <div className="project-links-md d-none d-md-block d-lg-none">
+            <a
+              // className="btn btn-social-icon btn-github"
+              href="https://github.com/eitanfire/legendary-quest.git"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <i className="fa fa-github"></i> GitHub
+            </a>
+          </div>
+        </Col>
+      </Row>
+      <h2 className="title">👻 Costume Contest Scoreboard</h2>
+      <Row className="mb-3">
+        <Col className="col">
+          <div className="project-links d-md-none d-lg-block">
+            <a
+              href="https://teach-league.web.app/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <i className="fa fa-circle-play"></i> Live Demo
+            </a>
+          </div>
+          <br></br>
+          <div className="project-links d-md-none d-lg-block">
+            <a
+              // className="btn btn-social-icon btn-github"
+              href="https://github.com/eitanfire/legendary-quest.git"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <i className="fa fa-github"></i> GitHub
+            </a>
+          </div>
+        </Col>
+        <Col
+        // className="vstack"
+        >
+          <Col
+            className="col col-12 
+        hstack"
+          >
+            <div className="project-links-md d-none d-md-block d-lg-none">
+              <a
+                href="https://teach-league.web.app/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <i className="fa fa-circle-play"></i> Live Demo
+              </a>
+            </div>
+            <br></br>
+            <div className="project-links-md d-none d-md-block d-lg-none">
+              <a
+                // className="btn btn-social-icon btn-github"
+                href="https://github.com/eitanfire/legendary-quest.git"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <i className="fa fa-github"></i> GitHub
+              </a>
+            </div>
           </Col>
-        </Col> */}
+          <Col className="screenshot-container col d-md-none d-xl-block col-xl-2">
+            <Col>
+              <img
+                className="screenshots"
+                src={Leaderboard}
+                alt="Teach League screenshot"
+              ></img>
+            </Col>
+          </Col>
+          <Col className="hstack">
+            <Col className="d-none d-md-block d-xl-none">
+              <img
+                className="screenshots-md"
+                src={Leaderboard}
+                alt="Teach League screenshot"
+              ></img>
+            </Col>
+            <br></br>
+          </Col>
+        </Col>
+        {/* </Row>
+      <Row> */}
+        <Col className="description col col-12 col-xl-8">
+          <p>
+            I aim to write clean code in a functional style but I won't hesitate
+            to use class-based components when working with legacy code such as
+            the Scoreboard which I started with a tutorial. As a teacher, a
+            whiteboard coding interview is not quite as terrifying as having a
+            school full of students tensely watching me debug a program to find
+            out the costume contest winner!
+          </p>
+        </Col>
       </Row>
     </Container>
   );
