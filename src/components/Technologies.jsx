b44850d0fcafe667b1fcdf78f92c2a4855e3fa8b
Eitan Fire <eitanfire@gmail.com>
2024-02-27 17:24:46 -0700

    update and restyle technologies

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -3,23 +3,23 @@ import { Container, Col, Row } from "reactstrap";
 const Technologies = () => {
   return (
     <Container>
-      <Row>
-        <h2 className="technologies-title">Latest Technologies</h2>
+      <h2 className="technologies-title">Latest Technologies</h2>
+      <Row className="technologies">
         <Col>
           <section className="technologies-section">
-            <h2 className="technologies-subsection-title">• Front-End</h2>◊
-            HTML, CSS, JavaScript, JSX, Redux
+            <h2 className="technologies-subsection-title">Front-End</h2>
+            HTML, CSS, SCSS, JavaScript, JSX, Redux
           </section>
         </Col>
         <Col>
           <section className="technologies-section">
-            <h2 className="technologies-subsection-title">• Frameworks</h2>◊
+            <h2 className="technologies-subsection-title">Frameworks</h2>
             React, React Native, Ruby on Rails, Bootstrap
           </section>
         </Col>
         <Col>
           <section className="technologies-section">
-            <h2 className="technologies-subsection-title">• Back-End</h2>◊
+            <h2 className="technologies-subsection-title">Back-End</h2>
             NodeJS, Express, Firebase, Ruby, API, RESTful web services
           </section>
         </Col>
