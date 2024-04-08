21d6ca7bb035ab7d77122563128d7b9f744ba2a4
Eitan Fire <eitanfire@gmail.com>
2024-04-08 17:23:35 -0600

    refactor contact-links as ContactInfo

--- src/App.jsx
+++ src/App.jsx
@@ -5,54 +5,13 @@ import Leaderboard from "./assets/img/leaderboard--ss.png";
 import { Container, Col, Row, Card, CardBody } from "reactstrap";
 import Projects from "./components/Projects";
 import Technologies from "./components/Technologies";
+import ConactInfo from "./components/ContactInfo";
 import "./App.css";
-
-const handleCopyClick = (event) => {
-  event.preventDefault(); // Prevent the default behavior of the anchor tag
-
-  const emailInput = document.getElementById("emailInput");
-  emailInput.select();
-  document.execCommand("copy");
-  emailInput.setSelectionRange(0, 0);
-
-  window.alert("Email address copied!"); // Show a popup message
-};
 
 function App() {
   return (
     <Container>
-      <contact-links>
-        <a
-          href="https://drive.google.com/file/d/15u2y7JFO3BUc1gGXpk9bGd8-bUxUUpDG/view?usp=sharing"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <i class="fa-solid fa-image-portrait"></i> Resume
-        </a>
-        {" • "}
-        <a href="https://github.com/eitanfire" target="_blank" rel="noreferrer">
-          <i className="fa fa-github"></i> GitHub
-        </a>
-        {" • "}
-        <a
-          href="https://www.linkedin.com/in/eitanfire/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <i className="fa fa-linkedin"></i> LinkedIn
-        </a>
-        {" • "}
-        <a href="tel:+13038177843">
-          <i className="fa fa-phone"></i> Phone
-        </a>
-        {" • "}
-        <a href="mailto:eitanfire@gmail.com" target="_blank" rel="noreferrer">
-          <i className="fa fa-envelope-o"></i> Email
-        </a>{" "}
-        <a href="mailto:eitanfire@gmail.com" id="emailInput">
-          <i className="fa fa-copy" onClick={handleCopyClick} />
-        </a>
-      </contact-links>
+      <ConactInfo />
       <h1 className="main-title">My Latest Projects</h1>
       <p className="intro-text">
         My name is Eitan Fire I am a motivated, self-starting, and collaborative
@@ -70,6 +29,7 @@ function App() {
       </p>
       <Technologies />
       <Projects className="" />
+      <ConactInfo />
     </Container>
   );
 }
---
+++ src/components/ContactInfo.jsx
@@ -0,0 +1,49 @@
+const handleCopyClick = (event) => {
+  event.preventDefault(); // Prevent the default behavior of the anchor tag
+
+  const emailInput = document.getElementById("emailInput");
+  emailInput.select();
+  document.execCommand("copy");
+  emailInput.setSelectionRange(0, 0);
+
+  window.alert("Email address copied!"); // Show a popup message
+};
+
+const ContactInfo = () => {
+  return (
+    <contact-links>
+      <a
+        href="https://drive.google.com/file/d/15u2y7JFO3BUc1gGXpk9bGd8-bUxUUpDG/view?usp=sharing"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <i className="fa-solid fa-image-portrait"></i> Resume
+      </a>
+      {" • "}
+      <a href="https://github.com/eitanfire" target="_blank" rel="noreferrer">
+        <i className="fa fa-github"></i> GitHub
+      </a>
+      {" • "}
+      <a
+        href="https://www.linkedin.com/in/eitanfire/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <i className="fa fa-linkedin"></i> LinkedIn
+      </a>
+      {" • "}
+      <a href="tel:+13038177843">
+        <i className="fa fa-phone"></i> Phone
+      </a>
+      {" • "}
+      <a href="mailto:eitanfire@gmail.com" target="_blank" rel="noreferrer">
+        <i className="fa fa-envelope-o"></i> Email
+      </a>{" "}
+      <a href="mailto:eitanfire@gmail.com" id="emailInput">
+        <i className="fa fa-copy" onClick={handleCopyClick} />
+      </a>
+    </contact-links>
+  );
+};
+
+export default ContactInfo;
