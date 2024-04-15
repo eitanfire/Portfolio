16db277b45c277ade7bb8560f388b64bf5ddfd83
Eitan Fire <eitanfire@gmail.com>
2024-04-14 23:49:01 -0600

    prepare to map over icons

--- src/components/IconsList.jsx
+++ src/components/IconsList.jsx
@@ -1,31 +1,25 @@
 import React from "react";
 import { Container, Col, Row } from "reactstrap";
+import reduxIcon from "../assets/img/icons8-redux-50.png";
+import firestoreIcon from "../assets/img/icons8-firestore-48.png";
+import nodejsIcon from "../assets/img/icons8-node-js-32.png";
+import calendarIcon from "../assets/img/icons8-google-calendar-50.png";
 
 const IconList = () => {
-  // Define your list of icons
   const icons = [
     { className: "fa-brands fa-html5", text: "HTML" },
     { className: "fa-brands fa-bootstrap", text: "Bootstrap" },
     { className: "fa-brands fa-css3-alt", text: "CSS" },
     { className: "fa-brands fa-js", text: "Javascript" },
     { className: "fa-brands fa-react", text: "React" },
+    { className: "fa-brands fa-node fa-lg", text: "NodeJS" },
+    { className: "png-icon", text: "Express", image: nodejsIcon },
+    { className: "png-icon", text: "Redux", image: reduxIcon },
+    { className: "png-icon", text: "Firestore", image: firestoreIcon,},
+    { className: "fa-solid fa-lock", text: "Firebase Auth" },
+    { className: "png-icon", text: "Google Calendar API", image: calendarIcon },
+    { className: "fa fa-github", text: "GitHub" },
   ];
-
-  return (
-    <div>
-      {/* Map over the list of icons and render them */}
-      {icons.map((icon, index) => (
-        <div key={index}>
-          {/* Render the icon */}
-
-          <i className={icon.className}></i>
-
-          {/* Render the text */}
-          {icon.text}
-        </div>
-      ))}
-    </div>
-  );
-};
-
+}
 export default IconList;
+
