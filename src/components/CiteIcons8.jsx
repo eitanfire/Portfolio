7bb7c9d68ac2a0b4d10c3000b1b1cd29b2828249
Eitan Fire <eitanfire@gmail.com>
2024-06-10 22:06:18 -0600

    secure links

--- src/components/CiteIcons8.jsx
+++ src/components/CiteIcons8.jsx
@@ -4,12 +4,17 @@ const CiteIcons8 = () => {
   return (
     <Container>
       <cite>
-        <a target="_blank" href="https://icons8.com/icon/FQlr_bFSqEdG/node-js">
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://icons8.com/icon/FQlr_bFSqEdG/node-js"
+        >
           Node Js
         </a>
         {", "}
         <a
           target="_blank"
+          rel="noreferrer"
           href="https://icons8.com/icon/SQEEoDcpUD6c/cloud-firestore"
         >
           Firestore
@@ -17,20 +22,29 @@ const CiteIcons8 = () => {
         {", "}
         <a
           target="_blank"
+          rel="noreferrer"
           href="https://icons8.com/icon/UJGaYqXaHWdp/google-calendar"
         >
           Google Calendar
         </a>
         {", "}
-        <a target="_blank" href="https://icons8.com/icon/set/bard/family-ios">
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://icons8.com/icon/set/bard/family-ios"
+        >
           Google Gemini
         </a>
         {" & "}
-        <a target="_blank" href="https://icons8.com/icon/set/bard/family-ios">
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://icons8.com/icons/set/google-classroom"
+        >
           Google Classroom
         </a>{" "}
         icons by{" "}
-        <a target="_blank" href="https://icons8.com">
+        <a target="_blank" rel="noreferrer" href="https://icons8.com">
           Icons8
         </a>
       </cite>
