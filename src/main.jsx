e542b310cd5b7b4db2703924d162d3c73366f395
Eitan Fire <eitanfire@gmail.com>
2024-09-27 22:19:22 -0600

    delete comment

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import App from "./App.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-social/bootstrap-social.css";
 import "font-awesome/css/font-awesome.css";
-// import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
