b19c02ed48ab0452dcf68f3393cc42d3004438d6
Eitan Fire <eitanfire@gmail.com>
2024-11-02 10:08:30 -0600

    correct GraphSQL to GraphQL

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -5,7 +5,7 @@ const Technologies = () => {
     <>
       <h3>Technical Competencies</h3>
       <Container className="technologies">
-        AWS, CI/CD, CSS, Express, Firebase, Git, GitHub, GraphSQL, JavaScript,
+        AWS, CI/CD, CSS, Express, Firebase, Git, GitHub, GraphQL, JavaScript,
         JSON, JSX, NodeJS, NoSQL, React, React Native, Redux, RESTful web
         services, Ruby, Ruby on Rails, Semantic HTML, SolidJS, SST, TypeScript
         {/* <Row>
