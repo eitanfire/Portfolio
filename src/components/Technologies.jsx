ec318ccef9ece822340da74e035254bf6f79afba
Eitan Fire <eitanfire@gmail.com>
2024-11-16 11:45:51 -0700

    add PostgreSQL to technologies list

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -6,7 +6,7 @@ const Technologies = () => {
       <h3>Technical Competencies</h3>
       <Container className="technologies">
         AWS, CI/CD, CSS, Express, Firebase, Git, GitHub, GraphQL, JavaScript,
-        JSON, JSX, NodeJS, NoSQL, React, React Native, PostgREST, Redux, RESTful
+        JSON, JSX, NodeJS, NoSQL, React, React Native, PostgREST, PostgreSQL, Redux, RESTful
         web services, Ruby, Ruby on Rails, Semantic HTML, SolidJS, SST,
         TypeScript
         {/* <Row>
