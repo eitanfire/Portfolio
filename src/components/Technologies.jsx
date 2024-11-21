a9149f5e039fc89b8263ab3247cc2b9ab7718e13
Eitan Fire <eitanfire@gmail.com>
2024-11-20 21:14:27 -0700

    delete comments

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -9,29 +9,6 @@ const Technologies = () => {
         JSON, JSX, NodeJS, NoSQL, React, React Native, PostgREST, PostgreSQL, Redux, RESTful
         web services, Ruby, Ruby on Rails, Semantic HTML, SolidJS, SST,
         TypeScript
-        {/* <Row>
-        <Col>
-          <section className="technologies-section">
-            <h3 className="technologies-subsection-title">Frontend</h3>
-            React, TypeScript, JavaScript, JSX, Redux, CSS, Semantic HTML
-          </section>
-        </Col>
-        <Col className="col col-12 col-sm-6 col-md-4 col-xl-4">
-          <section className="technologies-section">
-            <h3 className="technologies-subsection-title">
-              Libraries, Frameworks & Version Control{" "}
-            </h3>
-            React, React Native, Ruby on Rails, Reactstrap, Bootstrap, Git,
-            GitHub
-          </section>
-        </Col>
-        <Col className="col col-12 col-md-4">
-          <section className="technologies-section">
-            <h3 className="technologies-subsection-title">Backend</h3>
-            NoSQL, NodeJS, Express, Firebase, Ruby, RESTful web services, JSON
-          </section>
-        </Col>
-      </Row> */}
       </Container>
     </>
   );
