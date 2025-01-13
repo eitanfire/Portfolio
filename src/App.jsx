1a8d1cbf8adca71089eda0475a34d12bd9c7f5c7
Eitan Fire <eitanfire@gmail.com>
2025-01-12 23:57:17 -0700

    format

--- src/App.jsx
+++ src/App.jsx
@@ -17,10 +17,9 @@ function App() {
         </span>
       </h1>
       <p className="intro-text">
-        I'm a software engineer with nearly 15 years of teaching experience.
-        {" "}{/*seeking a full-stack developer role.*/}
-        I am passionate about creating responsive and
-        mobile-first design websites and web applications.
+        I'm a software engineer with nearly 15 years of teaching experience.{" "}
+        {/*seeking a full-stack developer role.*/}I am passionate about creating
+        responsive and mobile-first design websites and web applications.
       </p>
       <Projects />
       <Technologies />
