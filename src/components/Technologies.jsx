fd8ebdd00dd301a353b98b116c8dbf0aee9b4834
Eitan Fire <eitanfire@gmail.com>
2025-02-07 21:37:40 -0700

    add python to technical competencies

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -6,7 +6,7 @@ const Technologies = () => {
       <h3 className="mt-3">Technical Competencies</h3>
       <Container className="technologies">
         AWS, CI/CD, CSS, Express, Firebase, Git, GitHub, GraphQL, JavaScript,
-        JSON, JSX, NodeJS, NoSQL, React, React Native, PostgREST, PostgreSQL, Redux, RESTful
+        JSON, JSX, NodeJS, NoSQL, React, React Native, PostgREST, PostgreSQL, Python, Redux, RESTful
         web services, Ruby, Ruby on Rails, Semantic HTML, SolidJS, SST, Supabase,
         TypeScript
       </Container>
