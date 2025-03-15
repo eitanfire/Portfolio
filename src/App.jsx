fd74fb2008e3d8c8fd18f17dcea71a31fe6c0cc2
Eitan Fire <eitanfire@gmail.com>
2025-03-14 21:01:21 -0600

    style Technologies section

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { MantineProvider } from "@mantine/core";
 import { Container } from "reactstrap";
 import Projects from "./components/Projects";
 import Technologies from "./components/Technologies";
@@ -7,25 +8,27 @@ import "./App.css";
 
 function App() {
   return (
-    <Container className="wrapper col col-12">
-      <ConactInfo />
-      <h1 className="main-title">
-        <span className="eitan-fires">Eitan Fire's</span>
-        {"\n"}
-        <span className="latest-projects">
-          Latest Pro<span className="j-from-projects">j</span>ects
-        </span>
-      </h1>
-      <p className="intro-text">
-        As a software engineer with a background in teaching, I craft
-        responsive, mobile-first web apps focused on intuitive user experiences
-        using modern frameworks.
-      </p>
-      <Projects />
-      <Technologies />
-      <CiteIcons8 />
-      <ConactInfo />
-    </Container>
+    <MantineProvider defaultColorScheme="auto" >
+      <Container className="wrapper col col-12">
+        <ConactInfo />
+        <h1 className="main-title">
+          <span className="eitan-fires">Eitan Fire's</span>
+          {"\n"}
+          <span className="latest-projects">
+            Latest Pro<span className="j-from-projects">j</span>ects
+          </span>
+        </h1>
+        <p className="intro-text">
+          As a software engineer with a background in teaching, I craft
+          responsive, mobile-first web apps focused on intuitive user
+          experiences using modern frameworks.
+        </p>
+        <Projects />
+        <Technologies />
+        <CiteIcons8 />
+        <ConactInfo />
+      </Container>
+    </MantineProvider>
   );
 }
 
--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -3,8 +3,8 @@ import { Container } from "reactstrap";
 const Technologies = () => {
   return (
     <>
-      <h3 className="mt-3">Technical Competencies</h3>
-      <Container className="technologies">
+      <h3 className="technologies-utilized-title ">Technical Competencies</h3>
+      <Container className="technologies p-4">
         AWS, CI/CD, CSS, Express, Firebase, Git, GitHub, GraphQL, JavaScript,
         JSON, JSX, NodeJS, NoSQL, React, React Native, PostgREST, PostgreSQL,
         Python, Redux, RESTful web services, Ruby, Ruby on Rails, Semantic HTML,
