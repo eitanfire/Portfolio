29ad25652dfa6f28496c134f5513dd89da11b682
Eitan Fire <eitanfire@gmail.com>
2025-03-28 12:30:19 -0600

    add technology

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -5,7 +5,7 @@ const Technologies = () => {
     <>
       <h3 className="technologies-utilized-title ">Technical Competencies</h3>
       <Container className="technologies p-4">
-        AWS, CI/CD, CSS, Express, Firebase, Git, GitHub, GraphQL, JavaScript,
+        Angular, AWS, CI/CD, CSS, Express, Firebase, Git, GitHub, GraphQL, JavaScript,
         JSON, JSX, NodeJS, NoSQL, React, React Native, PostgREST, PostgreSQL,
         Python, Redux, RESTful web services, Ruby, Ruby on Rails, Semantic HTML,
         SolidJS, SST, Supabase, TypeScript
