e5461c791e30322083047f2b69182ad644cc78bc
Eitan Fire <eitanfire@gmail.com>
2025-04-09 16:04:10 -0600

    update technologies

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -5,8 +5,8 @@ const Technologies = () => {
     <>
       <h3 className="technologies-utilized-title ">Technical Competencies</h3>
       <Container className="technologies p-4">
-        Angular, AWS, CI/CD, CSS, Express, Firebase, Git, GitHub, GraphQL, JavaScript,
-        JSON, JSX, NodeJS, NoSQL, React, React Native, PostgREST, PostgreSQL,
+        AWS, CI/CD, CSS, Express, Firebase, Git, GitHub, GraphQL, JavaScript,
+        JSON, JSX, Mantine UI, NodeJS, NoSQL, React, React Native, PostgREST, PostgreSQL,
         Python, Redux, RESTful web services, Ruby, Ruby on Rails, Semantic HTML,
         SolidJS, SST, Supabase, TypeScript
       </Container>
