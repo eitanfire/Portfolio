68f09250a0ee50424ad958ba7d5cd30c3d133f82
Eitan Fire <eitanfire@gmail.com>
2025-04-24 22:20:36 -0600

    delete comments

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,4 +1,3 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 
@@ -12,6 +11,5 @@ const firebaseConfig = {
   measurementId: "G-DMWDJNBX85",
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
